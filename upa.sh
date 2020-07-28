daa05832bf478100a60978d277b6f02420e34ffe
bazilio <carlosbazilio@gmail.com>
2020-07-28 00:19:11 -0300

    Versão inicial

---
+++ upa.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/anaconda3/bin/python
+. env/bin/activate
+python estadao2rss.py
+git add *.xml
+dia=date+"%d/%m/%Y"
+git commit -m "Atualização Automática - $dia"
+git push
